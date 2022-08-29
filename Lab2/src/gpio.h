d6feb8e6827c2aad729f132fb6c519466725d58c
pieraqua <pauloffxii@hotmail.com>
2022-08-28 22:45:32 -0300

    led ascende

--- Lab2/src/gpio.h
+++ Lab2/src/gpio.h
@@ -10,7 +10,13 @@
 #include "inc/hw_memmap.h"
 
 #define USER_LED1 GPIO_PIN_0 //PN0
+#define USER_LED2 GPIO_PIN_1 //PN1
 #define USER_SWITCH GPIO_PIN_0 //PJ0
+
+typedef enum{
+    LED_OFF = 0,
+    LED_ON = 0xFF
+}enLEDState;
 
 /**
 * Inicializa as portas GPIO utilizadas.
@@ -26,6 +32,11 @@ void vGPIO_BlinkLED();
 * @brief Retorna o estado da flag do user_switch
 */
 uint8_t ui8GPIO_GetSwitchFlag();
+
+/**
+* @brief Seta o estado do USER_LED1.
+*/
+void vGPIO_SetLED1(enLEDState val);
 
 /**
 * @brief Seta o estado da flag do user_switch para @p val.
