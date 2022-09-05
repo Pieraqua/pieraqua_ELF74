cf1c41b79a636ab4a89ac9b8ac4a54496d3849ba
Cássio Yamauchi <cassio.yamauchi@gmail.com>
2022-09-04 22:57:43 -0300

    demo

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -1,21 +1,14 @@
 #include <stdint.h>
 #include <iostream>
 using namespace std;
-
-extern const uint8_t * const p_start_image0;
-extern const uint8_t** image1;
-const uint8_t * const p_start_image1 = &image1[0][0];
-#define WIDTH  160
-#define HEIGHT 120
+#include <images.h>
 
 extern "C" uint16_t EightBitHistogram(uint16_t width, uint16_t height, uint8_t const *p_image, uint16_t *p_histogram);
 
 int main()
 {
   uint16_t histogram[256];
-  int width = WIDTH;
-  int height = HEIGHT;
-  int pixels = EightBitHistogram(width, height, p_start_image1, histogram);
+  int pixels = EightBitHistogram(width1, height1, &image1[0][0], histogram);
   cout << pixels << endl;
   return 0;
 }
--- Lab3_images/images.h
+++ Lab3_images/images.h
@@ -6,6 +6,6 @@ extern const uint16_t width0;
 extern const uint16_t height0;
 extern const uint8_t * const p_start_image0;
 
-extern const uint16_t width1  = WIDTH1;
-extern const uint16_t height1 = HEIGTH1;
-extern const uint8_t image1[HEIGTH1][WIDTH1];
+extern const uint16_t width1;
+extern const uint16_t height1;
+extern const uint8_t **image1;
