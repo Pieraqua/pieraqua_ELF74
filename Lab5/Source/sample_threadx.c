b209c3a5911aefbe6545401adb41109f7d00fbdf
Cássio Yamauchi <cassio.yamauchi@gmail.com>
2022-10-02 22:13:49 -0300

    lab5 modified

--- Lab5/Source/sample_threadx.c
+++ Lab5/Source/sample_threadx.c
@@ -4,6 +4,11 @@
    description of this demonstration.  */
 
 #include "tx_api.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/systick.h"
+#include "driverlib/sysctl.h"
+
 
 #define DEMO_STACK_SIZE         1024
 #define DEMO_BYTE_POOL_SIZE     9120
@@ -72,8 +77,17 @@ int main()
     
     /* Please refer to Chapter 6 of the ThreadX User Guide for a complete
        description of this demonstration.  */
-
-
+  
+    //
+    // Run from the PLL at 40 MHz.
+    // Note: SYSCTL_CFG_VCO_240 is a new setting provided in TivaWare 2.2.x and
+    // later to better reflect the actual VCO speed due to SYSCTL#22.
+    //
+    //ui32SysClock = 
+    SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+                                       SYSCTL_OSC_MAIN |
+                                       SYSCTL_USE_PLL |
+                                       SYSCTL_CFG_VCO_240), 120000000);
     /* Enter the ThreadX kernel.  */
     tx_kernel_enter();
 }
