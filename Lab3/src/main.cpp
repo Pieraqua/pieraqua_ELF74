af14aac3d1dbdff68e9466c404de602a830594dd
Pieraqua <pauloffxii@hotmail.com>
2022-10-03 09:25:56 -0300

    diagrama objs

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -8,7 +8,7 @@ extern "C" uint16_t EightBitHistogram(uint16_t width, uint16_t height, uint8_t c
 int main()
 {
   uint16_t histogram[256];
-  int pixels = EightBitHistogram(width0, height0, p_start_image0, histogram);
+  int pixels = EightBitHistogram(width1, height1, p_start_image1, histogram);
   //int pixels = EightBitHistogram(width0, height0, p_start_image0, histogram);
   cout << pixels << endl;
   for (int i = 0; i < 256; i++){
